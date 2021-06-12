9549d3a951b72916874b6e73b56b74040e0ec5dc
martin <mkenny5@tcd.ie>
2021-06-12 11:38:01 +0100

    Remove Haar cascades. Use Resnet50 to predict dog vs human

---
+++ app/main.py
@@ -0,0 +1,5 @@
+import cv2
+import keras
+import numpy as np
+import streamlit as st
+from PIL import Image
--- app/run.py
+++
@@ -1,4 +0,0 @@
-import keras
-import numpy as np
-from flask import Flask
-from flask import render_template, request, jsonify
