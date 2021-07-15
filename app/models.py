ef41a8e38933e9605b49a0f603665d3f09d484b6
Martin <59205886+kynnemall@users.noreply.github.com>
2021-07-15 21:46:26 +0100

    Third dimension needed perhaps

--- app/models.py
+++ app/models.py
@@ -23,9 +23,9 @@ def image_to_tensor(input_img, np_array=False):
     """
     if np_array:
         func_image = Image.fromarray(input_img)
-        img = np.resize(func_image, (224, 224))
+        img = np.resize(func_image, (224, 224, 3))
     else:
-        img = np.resize(input_img, (224, 224))
+        img = np.resize(input_img, (224, 224, 3))
     tensor = np.expand_dims(img, axis=0)
     return tensor
 
