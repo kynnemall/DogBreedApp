110b3011b389109b51593330fe34798f3f91dbe5
Martin <59205886+kynnemall@users.noreply.github.com>
2021-07-15 20:18:13 +0100

    Use TOML format

--- app/main.py
+++ app/main.py
@@ -12,7 +12,7 @@ from lime import run_lime
 st.set_page_config(
      page_title="Dog breed guesser"
 )
-track_tag = st.secrets["analytics_tag"]
+track_tag = st.secrets["ANALYTICS_TAG"]
 
 components.html(
     f"""
